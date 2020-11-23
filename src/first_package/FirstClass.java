14a94f7199fd2b191ab5ac1e7cd371de1a817b1b
AOKanalyst <aokanalyst@gmail.com>
2020-11-23 12:27:33 -0500

    last commit from local master branch

--- src/first_package/FirstClass.java
+++ src/first_package/FirstClass.java
@@ -6,6 +6,9 @@ public class FirstClass {
 		System.out.println("Our first commit from local repo");
 		System.out.println("Our second commit from local repository");
 		System.out.println("Now I am on my AOKtester branch");
+		System.out.println("I want to have a conflict");
+		
+		System.out.println("I want to have a second conflict");
 	}
 
 }
